83af6e910fc4ef8237e4c76af1f1a0305b27ba62
Rua Abdul Majeed Yousuf AlBalushi <ruaabolushi@gmail.com>
2024-11-23 16:45:39 +0400

    Added MeaningCloud API integration and test cases

---
+++ src/js/sentiment.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const  getSentimentAnalysis = async(text) => {
+    const apiKey = process.env.API_KEY;
+    const url = 'https://api.meaningcloud.com/sentiment-2.1';
+    try {
+        const response = await axios.post(url, null, {
+            params: {
+                key: apiKey,
+                txt: text,
+                lang: 'en'
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('error while calling Meaningcloud API:', error);
+        throw error;
+    
+}
+};
+module.exports = { getSentimentAnalysis };
+
+
---
+++ src/js/sentiment.test.js
@@ -0,0 +1,31 @@
+
+const { getSentimentAnalysis}= require('../sentiment');
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+test('should return sentiment analysis for input text and classify it as positive ', async () =>{
+    const text = 'i love programming!';
+    const result = await getSentimentAnalysis(text);
+    ecpect(result).toHaveProperty('score_tag');
+    expect(result.score_tag).toBe('p');
+});
+
+test('should return sentiment analysis for input text and classify it as negative ', async () =>{
+    const text = 'i hate buges!';
+    const result = await getSentimentAnalysis(text);
+    ecpect(result).toHaveProperty('score_tag');
+    expect(result.score_tag).toBe('N');
+});
+
+
+test('should return sentiment analysis for input text and classify it as Neutral ', async () =>{
+    const text = 'this is normal sentence ';
+    const result = await getSentimentAnalysis(text);
+    ecpect(result).toHaveProperty('score_tag');
+    expect(result.score_tag).toBe('NEU');
+});
+
+
+
+
